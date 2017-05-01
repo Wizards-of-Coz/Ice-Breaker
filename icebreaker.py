dd6ac75aaf7b8171280d92c59dbecc3fe6497072
Yuchen Huang <oocasthy@umich.edu>
2017-05-01 17:58:14 -0400

    Add comments

--- icebreaker.py
+++ icebreaker.py
@@ -8,11 +8,15 @@ from cozmo.util import degrees, distance_mm, speed_mmps
 from cozmo.lights import Light
 from questions import Questions
 
+# maximum head angle for looking up faces
 MAX_HEAD_ANGLE = cozmo.robot.MAX_HEAD_ANGLE
+# minimum head angle for looking up faces
 MIN_HEAD_ANGLE = degrees(20)
+# tiny gap to work around sharp maximum error
 ANGLE_EPSILON = degrees(0.5)
 TURN_SPEED = 20
 STRAIGHT_SPEED = 25
+# distance close up to human
 DISTANCE_HUMAN = 80
 TURN_ANGLE_AFTER_INTERACTION = degrees(90)
 
@@ -24,6 +28,7 @@ MIN_PITCH = -0.4
 MAX_PITCH = -0.7
 
 # duration: question length == 10, duration = 1, question length = 100, duration = 2
+# in seconds
 MIN_DURATION = 1
 MAX_DURATION = 2
 
@@ -47,6 +52,7 @@ class IceBreaker(WOC):
         self._robot = await coz_conn.wait_for_robot()
         self._initPose = self._robot.pose
         self._cube = None
+        # start with the "next question button" cube placed in front of cozmo
         try:
             self._cube = await self._robot.world.wait_for_observed_light_cube(timeout=30)
             print("Found cube: %s" % self._cube)
@@ -55,15 +61,18 @@ class IceBreaker(WOC):
 
         while True:
             if not self._face:
+                # first spin fast, then spin slower to seek human face
                 await self.crazySpin()
                 await self.spin()
             else:
+                # find a face, do interaction
                 await self.moveCloser()
                 await self.coreInteraction()
                 await self.waitFinishInteraction()
                 await self.afterInteraction()
             await asyncio.sleep(0.1)
         
+    # spin to find any human face
     async def spin(self):
         # start spin
         if not self._robot.is_moving:
@@ -74,6 +83,7 @@ class IceBreaker(WOC):
             await spinAction.wait_for_completed()
             cozmo.logger.info("spinAction = %s", spinAction)
 
+    # close up to human whose face is just seen by Cozmo
     async def moveCloser(self):
         # turn to human Cozmo saw
         await self._robot.turn_towards_face(self._face).wait_for_completed()
@@ -90,6 +100,7 @@ class IceBreaker(WOC):
         await action2.wait_for_completed()
         cozmo.logger.info("action2 = %s", action2)
         
+    # ask ice breaking question
     async def coreInteraction(self):
         self._question = self._questions.getRandomQuestion()
         (pitch, self._duration) = await self.customizeSpeech(self._question)
@@ -101,6 +112,7 @@ class IceBreaker(WOC):
                                    voice_pitch=pitch,
                                    duration_scalar=self._duration).wait_for_completed()
 
+    # repeat question if repeating button is pressed
     async def repeatInteraction(self):
         pitch = random.uniform(MIN_PITCH, MAX_PITCH)
         await self._robot.say_text(self._question,
@@ -109,6 +121,7 @@ class IceBreaker(WOC):
                                    voice_pitch=pitch,
                                    duration_scalar=self._duration).wait_for_completed()
     
+    # use cubes to finish interaction with human
     async def waitFinishInteraction(self):
         finishConfirmed = False
         while not finishConfirmed:
@@ -117,6 +130,8 @@ class IceBreaker(WOC):
             await self._robot.set_head_angle(degrees(0),
                                              in_parallel=True).wait_for_completed()
             await self._robot.play_anim_trigger(cozmo.anim.Triggers.MeetCozmoGetIn).wait_for_completed()
+            
+            # wait to see a cube
             try:
                 cube = await self._robot.world.wait_for_observed_light_cube(timeout=30)
                 print("Found cube: %s" % cube)
@@ -125,6 +140,7 @@ class IceBreaker(WOC):
             finally:
                 pass
 
+            # found ANY cube
             if cube:
                 # set cube flashing light
                 cube.set_lights(CUBE_READY_LIGHT)
@@ -135,6 +151,8 @@ class IceBreaker(WOC):
                 # finishConfirmed = True
 
                 tapped = None
+
+                # wait this cube to be tapped
                 try:
                     tapped = await cube.wait_for_tap(timeout=30)
                     print("Cube tapped: %s" % tapped)
@@ -142,11 +160,14 @@ class IceBreaker(WOC):
                     print("Didn't tap the cube")
 
                 if tapped:
+                    # show the cube is tapped
                     cube.set_lights(cozmo.lights.green_light)
                     
+                    # the tapped cube is the cube of "next question"
                     if cube == self._cube:
                         finishConfirmed = True
                         await self._robot.play_anim_trigger(cozmo.anim.Triggers.BuildPyramidSuccess).wait_for_completed()
+                    # other cube
                     else:
                         await self.repeatInteraction()
                     cube.set_lights_off()
@@ -154,7 +175,7 @@ class IceBreaker(WOC):
                 # didn't find cube
                 await self._robot.play_anim_trigger(cozmo.anim.Triggers.DriveStartAngry).wait_for_completed()
             
-
+    # return to spin position to start next round
     async def afterInteraction(self):
         await self._robot.go_to_pose(self._initPose).wait_for_completed()
 ##        await self._robot.turn_in_place(TURN_ANGLE_AFTER_INTERACTION).wait_for_completed()
@@ -163,13 +184,14 @@ class IceBreaker(WOC):
 ##                                         speed_mmps(STRAIGHT_SPEED),
 ##                                         should_play_anim=False).wait_for_completed()
 
+    # customize speech voice pitch and length
     async def customizeSpeech(self, question):
         pitch = random.uniform(MIN_PITCH, MAX_PITCH)
         length = len(question) - question.count(' ')
         duration = log_mapping(10, MIN_DURATION, 100, MAX_DURATION, length, 10)
         return (pitch, duration)
         
-
+    # fast spin, don't care about faces
     async def crazySpin(self):
         if self._newRound:
             t = random.uniform(MIN_CRAZY_SPIN_TIME, MAX_CRAZY_SPIN_TIME)
--- questions.py
+++ questions.py
@@ -1,17 +1,19 @@
 import random
 
+# path to question file
 QUESTION_PATH = "Resource/questions.txt"
 
 class Questions:
     
     def __init__(self):
         self._questionArray = []
+        # load all questions from disk to memory
         f = open(QUESTION_PATH, 'r')
         for line in f:
             q = line[0:len(line)-1]
             self._questionArray.append(q)
 
-
+    # get one question randomly. May be the same one as the previous question
     def getRandomQuestion(self):
         i = random.randrange(len(self._questionArray))
         return self._questionArray[i]
